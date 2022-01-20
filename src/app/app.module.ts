62a41e525db4d8c310ec06255aa458aefb8e7b72
Gabriel Fabro <fabrogabrielandres@gmail.com>
2022-01-20 16:40:47 -0600

    form Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
 @NgModule({
 	declarations: [ AppComponent ],
-	imports: [ BrowserModule, HeroesModule,ContadorModule ],
+	imports: [ BrowserModule, HeroesModule,ContadorModule, DbzModule ],
 	providers: [],
 	bootstrap: [ AppComponent ]
 })
